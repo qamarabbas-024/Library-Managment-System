b56e67d26af103d10afe72b714aab33204162a59
“qamarabbas024” <qamarabbas33333333@gmail.com>
2026-01-01 16:30:45 +0500

    member functions declared

---
+++ members.h
@@ -0,0 +1,15 @@
+#ifndef MEMBERS_H
+#define MEMBERS_H
+
+#include <string>
+using namespace std;
+
+void listMembers();
+void addMember();
+void editMember();
+void deleteMember();
+void banMember();
+void unbanMember();
+void updateMemberInfo(const string &uid);
+
+#endif
