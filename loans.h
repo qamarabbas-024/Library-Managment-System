7b2466e4374ceb9f3442f68bc84fae0cd235d1ff
“Safi <sh2320193@gmail.com>
2026-01-02 19:20:57 +0500

    loan function added

---
+++ loans.h
@@ -0,0 +1,13 @@
+#ifndef LOANS_H
+#define LOANS_H
+
+#include <string>
+using namespace std;
+
+void applyLoan(const string &uid);
+void returnBook(const string &uid);
+void approveLoans();
+void memberActivities();
+
+#endif
+
